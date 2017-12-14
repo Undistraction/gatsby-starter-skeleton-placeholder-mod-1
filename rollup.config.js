571ebe4a710dfa759e1e020f3e313a4b319b719a
Pedr Browne <pedr.browne@gmail.com>
2017-12-14 23:20:48 +0000

    fix(rollup): Add replace plugin to set React process.env.NODE_ENV to ‘production’

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
 export default {
   name: 'gatsby-starter-skeleton-placeholder-mod 1',
@@ -12,6 +13,9 @@ export default {
   },
   plugins: [
     nodeResolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
     babel({
       babelrc: false,
       exclude: 'node_modules/**',
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line
 import React from 'react';
 
-export default context => {
+export default ({ context }) => {
   context.style.backgroundColor = '#FF0000';
   ReactDOM.render(<div>Code Run From Installed NPM Module</div>, context);
 };
